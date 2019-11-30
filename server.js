e7d8bb3bba4f778c9e178985e2608a63a3380537
ssssyed98 <ssssyed98@gmail.com>
2019-11-30 23:26:13 +0500

    Updated package.json files for server

--- server.js
+++ server.js
@@ -16,10 +16,7 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profiles", require("./routes/api/profiles"));
 app.use("/api/posts", require("./routes/api/posts"));
-app.use(
-  "/api/crowdfunding/campaign",
-  require("./routes/api/crowdfunding/campaign")
-);
+app.use("/api/crowdfunding/campaign", require("./routes/api/crowdfunding/campaign"));
 
 const PORT = process.env.PORT || 5000;
 
