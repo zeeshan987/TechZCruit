6eb470699b1b6a6e6bf22c375ec3ce801dbe46b8
ssssyed98 <ssssyed98@gmail.com>
2019-12-06 12:34:50 +0500

    Now able to update and delete a group in the backend

--- routes/api/community/groups.js
+++ routes/api/community/groups.js
@@ -5,10 +5,27 @@ const { check, validationResult } = require('express-validator');
 const Group = require('../../../models/community/Group');
 
 // @route   GET /api/community/groups
-// @desc    Test Route
+// @desc    Get all groups
 // @access  Public
-router.get('/', (req, res) => {
-  res.send('Groups route');
+router.get('/', async (req, res) => {
+  try {
+    const groups = await Group.find();
+    res.json(groups);
+  } catch (err) {
+    return res.status(500).send('Server error');
+  }
+});
+
+// @route   GET /api/community/groups/:id
+// @desc    Get group by id
+// @access  Public
+router.get('/:id', async (req, res) => {
+  try {
+    const group = await Group.findOne({ _id: req.params.id });
+    res.json(group);
+  } catch (err) {
+    return res.status(500).send('Server error');
+  }
 });
 
 // @route   POST /api/community/groups
@@ -44,5 +61,79 @@ router.post(
     }
   }
 );
+
+// @route   PUT /api/community/groups/:id
+// @desc    Update a group
+// @access  Private
+router.put(
+  '/:id',
+  [
+    auth,
+    check('name', 'Name is required')
+      .not()
+      .isEmpty()
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    let group = await Group.findById(req.params.id);
+
+    if (!group) {
+      return res.status(400).json({ msg: 'Group does not exist' });
+    }
+
+    if (group.admin.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+
+    const { name, description } = req.body;
+
+    try {
+      group = await Group.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $set: {
+            name,
+            description
+          }
+        },
+        { new: true }
+      );
+
+      await res.json(group);
+    } catch (err) {
+      return res.status(500).send('Server error');
+    }
+  }
+);
+
+// @route   DELETE /api/community/groups/:id
+// @desc    Delete a group
+// @access  Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const group = await Group.findById(req.params.id);
+
+    if (!group) {
+      return res.status(400).json({ msg: 'Group does not exist' });
+    }
+
+    if (group.admin.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+
+    try {
+      await group.remove();
+      res.json({ msg: 'Group removed' });
+    } catch (err) {
+      return res.status(500).send('Server error');
+    }
+  } catch (err) {
+    return res.status(500).send('Server error');
+  }
+});
 
 module.exports = router;
