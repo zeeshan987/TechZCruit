31765cb993f742ea2d3e3688a822af7ad2be5f43
ssssyed98 <ssssyed98@gmail.com>
2020-01-19 11:56:19 +0500

    Updated UI of groups page by adding tabs

--- client/src/components/community/group/GroupNavigationTabs.js
+++ client/src/components/community/group/GroupNavigationTabs.js
@@ -1,11 +1,37 @@
 import React, { Fragment } from 'react';
-import { Nav } from 'react-bootstrap';
+import { Tab, Row, Col, Nav } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 const GroupNavigationTabs = ({ group }) => {
   return (
     <Fragment>
-      <Nav justify variant='tabs'>
+      <Tab.Container defaultActiveKey='posts'>
+        <Row>
+          <Col md={12}>
+            <Nav justify variant='tabs'>
+              <Nav.Item>
+                <Nav.Link eventKey='posts'>Posts</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey='members'>Members</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link eventKey='requests'>Requests</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12}>
+            <Tab.Content>
+              <Tab.Pane eventKey='posts'>Posts</Tab.Pane>
+              <Tab.Pane eventKey='members'>Members</Tab.Pane>
+              <Tab.Pane eventKey='requests'>Requests</Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
+      {/* <Nav justify variant='tabs'>
         <Nav.Item>
           <Nav.Link
             href={`/community/group/${group !== null ? group._id : ''}`}
@@ -19,7 +45,7 @@ const GroupNavigationTabs = ({ group }) => {
         <Nav.Item>
           <Nav.Link>Requests</Nav.Link>
         </Nav.Item>
-      </Nav>
+      </Nav> */}
     </Fragment>
   );
 };
