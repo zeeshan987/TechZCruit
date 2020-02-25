f11cf38505e9997525a81799458df4c65cfa3f51
ssssyed98 <ssssyed98@gmail.com>
2020-02-25 21:10:46 +0500

    App now merged and working only css needs to be fixed

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,13 +1,12 @@
-import { combineReducers } from "redux";
-import alert from "./alert";
-import auth from "./auth";
-import profile from "./profile";
-import post from "./community/post";
-import group from "./community/group";
-import campaign from "./crowdfunding/campaign";
-import project from "./testing/project";
-import product from "./ecommerce/product";
-import { combineReducers } from "redux";
+import { combineReducers } from 'redux';
+import alert from './alert';
+import auth from './auth';
+import profile from './profile';
+import post from './community/post';
+import group from './community/group';
+import campaign from './crowdfunding/campaign';
+import project from './testing/project';
+import product from './ecommerce/product';
 
 export default combineReducers({
   alert,
