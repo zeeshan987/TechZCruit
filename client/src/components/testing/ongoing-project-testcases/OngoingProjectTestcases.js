032b5b9944e4abeb9e44d85a3e849f2cfec1235a
ssssyed98 <ssssyed98@gmail.com>
2020-03-17 10:34:01 +0500

    Updated ui of ongoing project testcases page in testing module

--- client/src/components/testing/ongoing-project-testcases/OngoingProjectTestcases.js
+++ client/src/components/testing/ongoing-project-testcases/OngoingProjectTestcases.js
@@ -7,6 +7,10 @@ import {
   passTestcaseForProject,
   failTestcaseForProject
 } from '../../../actions/testing/project';
+import styles from '../../../css/testing/ongoing-projects-testcases/style.module.css';
+import SideNav from '../../layout/SideNav';
+import Alert from '../../layout/Alert';
+import Footer from '../../layout/Footer';
 
 const OngoingProjectTestcases = ({
   project: { loading, project },
@@ -28,62 +32,72 @@ const OngoingProjectTestcases = ({
 
   return (
     <Fragment>
-      <h1 className='large text-primary'>Ongoing project test cases</h1>
-      <p className='lead'>
-        <i className='fas fa-user'></i> Below is a list of all the test cases of
-        the current project you have checked
-      </p>
-      {!loading && project !== null ? (
-        <Table striped hover>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Expected result</th>
-              <th>Status</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {project.testCases.map(testcase => (
-              <tr>
-                <td>{testcase.name}</td>
-                <td>{testcase.description}</td>
-                <td>{testcase.expectedResult}</td>
-                <td>
-                  {getUserIndex(testcase) === -1
-                    ? 'Not executed'
-                    : testcase.actualResults[getUserIndex(testcase)].status
-                    ? 'PASS'
-                    : 'FAIL'}
-                </td>
-                <td>
-                  <Button
-                    variant='success'
-                    className='m-1'
-                    onClick={() =>
-                      passTestcaseForProject(project._id, testcase._id)
-                    }
-                  >
-                    PASS
-                  </Button>
-                  <Button
-                    variant='danger'
-                    className='m-1'
-                    onClick={() =>
-                      failTestcaseForProject(project._id, testcase._id)
-                    }
-                  >
-                    FAIL
-                  </Button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      ) : (
-        <div className='lead'>No test cases found</div>
-      )}
+      <section className={styles.section}>
+        <SideNav styles={styles} />
+
+        <div className={styles.content}>
+          <Alert />
+          <div className={styles.heading}>
+            <i className='fas fa-user'></i> Ongoing project test cases
+          </div>
+          <div className={styles.sub_heading}>
+            Below is a list of all the test cases of the current project
+          </div>
+          {!loading && project !== null ? (
+            <Table striped hover>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Description</th>
+                  <th>Expected result</th>
+                  <th>Status</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {project.testCases.map(testcase => (
+                  <tr>
+                    <td>{testcase.name}</td>
+                    <td>{testcase.description}</td>
+                    <td>{testcase.expectedResult}</td>
+                    <td>
+                      {getUserIndex(testcase) === -1
+                        ? 'Not executed'
+                        : testcase.actualResults[getUserIndex(testcase)].status
+                        ? 'PASS'
+                        : 'FAIL'}
+                    </td>
+                    <td>
+                      <Button
+                        variant='success'
+                        className='m-1'
+                        onClick={() =>
+                          passTestcaseForProject(project._id, testcase._id)
+                        }
+                      >
+                        PASS
+                      </Button>
+                      <Button
+                        variant='danger'
+                        className='m-1'
+                        onClick={() =>
+                          failTestcaseForProject(project._id, testcase._id)
+                        }
+                      >
+                        FAIL
+                      </Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          ) : (
+            <div className={styles.sub_heading}>No test cases found</div>
+          )}
+        </div>
+      </section>
+
+      <Footer styles={styles} />
     </Fragment>
   );
 };
