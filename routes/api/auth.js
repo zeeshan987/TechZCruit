3deb9a36cd1dfa34f847581ba4394a890d6f19f2
ssssyed98 <ssssyed98@gmail.com>
2020-04-02 19:46:52 +0500

    Refactored the api for the auth profiles and users route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../../middleware/auth');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 const User = require('../../models/User');
 
 // @route   GET /api/auth
